ecf4ed4d6571f160ee8f29e74e6875bb173d3ea0
Diego Hernán Piccinini Lagos <diego@guiasrails.es>
2015-07-05 10:02:14 +0200

    Corrected netative matcher

--- features/step_definitions/negative_search_step.rb
+++ features/step_definitions/negative_search_step.rb
@@ -1,5 +1,5 @@
 Then(/^which should match "([^"]*)", "([^"]*)", "([^"]*)" and "([^"]*)"$/) do |arg1, arg2, arg3, arg4|
-    expect(@results).to  include( include( "Name" => (match(arg1) or match(arg2) or match(arg3) or match(arg4)) ) )
+    expect(@results).to  include( include( "Name" => (match(arg1) and match(arg2) and match(arg3) and match(arg4)) ) )
 end
 
 Then(/^not "([^"]*)", "([^"]*)" or "([^"]*)"\.$/) do |arg1, arg2, arg3|
